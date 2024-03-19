8122e482e1e370b34ea42d55882cb24fcf1488f3
winniegr <gerong1008@gmail.com>
2024-03-19 09:28:41 +0800

    Delete Earthquake directory

--- Earthquake/app/src/main/java/com/example/earthquake/view/RecyclerItemListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.earthquake.view
-
-import android.app.Activity
-import com.example.earthquake.viewmodel.Feature
-
-interface RecyclerItemListener {
-    fun onItemSelected(context: Activity, earthquakeItem : Feature)
-}
